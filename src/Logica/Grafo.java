3eb0b627890c22d7096567e9b95e6c2e61d797ef
alejandroacr <alejandro@axoneh.com>
2016-06-05 09:57:22 -0500

    correccion de las lineas

--- src/Logica/Grafo.java
+++ src/Logica/Grafo.java
@@ -10,6 +10,7 @@ public class Grafo {
 	public NodoGrafo[] listaNodos;
 	public ArrayList<Line2D> listaAdyacencia;
 	public ArrayList<Region> regiones;
+	public ArrayList<String> nombres;
 	
 	public Grafo() {
 		nodos=0;
@@ -33,28 +34,13 @@ public class Grafo {
 	
 	public void AgregarArista(int in1, int in2){
 		if(listaNodos[in1]!= null && listaNodos[in2]!=null){
-			Line2D l=new Line2D.Double();
 			NodoGrafo ng1=listaNodos[in1];
 			NodoGrafo ng2=listaNodos[in2];
 			double x1=ng1.pocX;
 			double y1=ng1.pocY;
 			double x2=ng2.pocX;
-			double y2=ng2.pocY;		
-			double m=(y2-y1);
-			m/=(x2-x1);
-			double b1=(y1-(m*x1));
-			if(x1<x2){
-				x1+=7;
-				x2-=7;
-			}
-			else{
-				x1-=7;
-				x2+=7;
-			}
-			y1=(m*x1)+b1;
-			y2=(m*x2)+b1;
-			l.setLine(x1,y1,x2,y2);
-			listaAdyacencia.add(l);
+			double y2=ng2.pocY;
+			agregarLinea(x1,y1,x2,y2);
 			Nodo n=listaNodos[in1].n;
 			if(listaNodos[in1].n==null){
 				listaNodos[in1].n=new Nodo(in2);
@@ -62,12 +48,14 @@ public class Grafo {
 			else{
 				Nodo ay=n;
 				while(n!=null){
+					if(n.index==in2){
+						return;
+					}
 					ay=n;
 					n=n.sig;
 				}
 				ay.sig=new Nodo(in2);
 			}
-			
 		}
 		else{
 			System.out.println("No se puede agregar adyacencia");
@@ -83,6 +71,7 @@ public class Grafo {
 				}
 			}
 		}
+		nombres = new ArrayList<String>();
 		regiones = new ArrayList<Region>();
 		for(int i=0;i<listaNodos.length; i++){
 			if(listaNodos[i]!=null){
@@ -91,47 +80,118 @@ public class Grafo {
 		}
 		Polygon p;
 		Region r;
+		String s;
 		NodoGrafo ng;
-		for(int i=0; i<regiones.size();i++){
-			r=regiones.get(i);
+		for(int i=0; i<nombres.size();i++){
+			s=nombres.get(i);
 			p=new Polygon();
-			for(int j=0;j<r.nombre.length();j++){
-				int ay=Integer.parseInt(r.nombre.toCharArray()[j]+"");
+			for(int j=0;j<s.length();j++){
+				int ay=Integer.parseInt(s.toCharArray()[j]+"");
 				ng=listaNodos[ay];
 				p.addPoint(ng.pocX,ng.pocY);
 			}
-			regiones.get(i).region=p;
+			r=new Region();
+			r.nombre=s;
+			r.region=p;
+			regiones.add(r);
 		}
 		
 		return 0;// todo salio bien
 	}
 	
-	private void buscarRegiones(int in1, int in2, String nom){
+	private String buscarRegiones(int in1, int in2, String nom){
 		NodoGrafo ng1=listaNodos[in1];
 		NodoGrafo ng2=listaNodos[in2];
 		Nodo n1=ng1.n;
 		Nodo n2=ng2.n;
-		if(in1<in2){
-			nom+=in2;
-			while(n2!=null){
-				n1=ng1.n;
-				while(n1!=null){
-					if(n2.index==n1.index){
-						Region r=new Region();
-						r.nombre=nom+n1.index+""+in1;
-						regiones.add(r);
-						return;
+			if(in1<in2){
+				nom+=in2;
+				while(n2!=null){
+					n1=ng1.n;
+					while(n1!=null){
+						if(n2.index==n1.index){
+							return nom+""+n1.index+""+in1;
+						}
+						n1=n1.sig;
 					}
-					n1=n1.sig;
+					n2=n2.sig;
 				}
-				n2=n2.sig;
 			}
-		}
 		n2=ng2.n;
 		while(n2!=null){
-			buscarRegiones(in1, n2.index,nom);
+			String s=buscarRegiones(in1, n2.index,nom);
+			if(s.length()>3){
+				nombres.add(s);
+			}
 			n2=n2.sig;
 		}
+		return "";
+	}
+	
+	private void agregarLinea(double x1, double y1, double x2, double y2){
+		double a,b,c,d;
+		double ayx1=0,ayx2=0;
+		double sx1,sx2,sy1,sy2;
+		double m=(y2-y1);
+		m/=(x2-x1);
+		double b1=(y1-(m*x1));
+		// primer punto
+		a=1+(m*m);
+		b=2*((m*b1)-x1-(m*y1));
+		c=(x1*x1)+(b1*b1)-(2*b1*y1)-400+(y1*y1);
+		d=(b*b)-(4*a*c);
+		ayx1=((-1)*b)+Math.sqrt(d);
+		ayx1=ayx1/(2*a);
+		ayx2=((-1)*b)-Math.sqrt(d);
+		ayx2=ayx2/(2*a);
+		if(x1>x2){
+			if(ayx1>ayx2){
+				sx1=ayx2;
+			}
+			else{
+				sx1=ayx1;
+			}
+		}
+		else{
+			if(ayx1>ayx2){
+				sx1=ayx1;
+			}
+			else{
+				sx1=ayx2;
+			}
+		}
+		sy1=(m*sx1)+b1;
+
+		// segundo punto
+		a=1+(m*m);
+		b=2*((m*b1)-x2-(m*y2));
+		c=(x2*x2)+(b1*b1)-(2*b1*y2)-400+(y2*y2);
+		d=(b*b)-(4*a*c);
+		ayx1=((-1)*b)+Math.sqrt(d);
+		ayx1/=(2*a);
+		ayx2=((-1)*b)-Math.sqrt(d);
+		ayx2/=(2*a);
+		if(x1>x2){
+			if(ayx1>ayx2){
+				sx2=ayx1;
+			}
+			else{
+				sx2=ayx2;
+			}
+		}
+		else{
+			if(ayx1>ayx2){
+				sx2=ayx2;
+			}
+			else{
+				sx2=ayx1;
+			}
+		}
+		sy2=(m*sx2)+b1;
+		// crear linea
+		Line2D l=new Line2D.Double();
+		l.setLine(sx1, sy1, sx2, sy2);
+		listaAdyacencia.add(l);
 	}
 	
 }
--- src/Vista/CanvasEntradaGrafo.java
+++ src/Vista/CanvasEntradaGrafo.java
@@ -4,10 +4,10 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.geom.Line2D;
 import java.util.Random;
 
 import Logica.Grafo;
-import Logica.Nodo;
 import Logica.NodoGrafo;
 
 public class CanvasEntradaGrafo extends Canvas {
@@ -46,32 +46,32 @@ public class CanvasEntradaGrafo extends Canvas {
 	}
 	
 	private void pintarGrafo(){
-		NodoGrafo ng,ay;
-		Nodo n;
-		graficas.setColor(Color.black);
-		for(int i=0;i<grafo.nodos;i++){
-			ng=grafo.listaNodos[i];
-			n=ng.n;
-			while(n!=null){
-				ay=grafo.listaNodos[n.index];
-				graficas.drawLine(ng.pocX, ng.pocY, ay.pocX, ay.pocY);
-				n=n.sig;
-			}
-		}
+		NodoGrafo ng;
+		Line2D l;
 		if(grafo.regiones.size()>0){
+			System.out.println();
+			System.out.println("Regiones");
 			for(int i=0; i<grafo.regiones.size();i++){
 				graficas.setColor(generarColor());
 				graficas.fillPolygon(grafo.regiones.get(i).region);
-				System.out.println("n: "+(i+1)+" - "+grafo.regiones.get(i).nombre);
+				System.out.println("n"+(i+1)+": "+grafo.regiones.get(i).nombre);
 			}
 		}
 		for(int i=0;i<grafo.nodos;i++){
 			ng=grafo.listaNodos[i];
 			graficas.setColor(Color.white);
-			graficas.fillOval(ng.pocX-10, ng.pocY-10, 20, 20);
+			graficas.fillOval(ng.pocX-20, ng.pocY-20, 40, 40);
 			graficas.setColor(Color.black);
-			graficas.drawOval(ng.pocX-10, ng.pocY-10, 20, 20);
+			graficas.drawOval(ng.pocX-20, ng.pocY-20, 40, 40);
 			graficas.drawString(ng.ver+"", ng.pocX-2, ng.pocY+3);
+		}
+		for(int i=0;i<grafo.listaAdyacencia.size(); i++){
+			l=grafo.listaAdyacencia.get(i);
+			graficas.setColor(Color.black);
+			graficas.drawLine((int)l.getX1(),(int)l.getY1(),(int)l.getX2(),(int)l.getY2());
+			graficas.setColor(Color.red);
+			graficas.fillOval((int)l.getX1()-3,(int)l.getY1()-3,6, 6);
+			graficas.fillOval((int)l.getX2()-3,(int)l.getY2()-3,6, 6);
 		}
 	}
 	
